4a09991be7c0d3efae4c207d13c91e516de4a305
Xavier <xavier.munoz.pisco@gmail.com>
2024-03-29 03:01:47 -0500

    cleaning code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,6 @@ import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
-  {
     path: '',
     redirectTo: 'digimon-list',
     pathMatch: 'full'
